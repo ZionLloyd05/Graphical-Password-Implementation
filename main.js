fbe9425f3a5b0392bf38a444b663d68b54c38de1
ZionLloyd05 <zionlloyd05@gmail.com>
2019-02-05 07:26:58 +0100

    image drag 'n drop feature

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
-window.addEventListener('load', function () {
-    generate()
+let dropArea = document.getElementById('grids')
 
-    // $("#grids").on("click", ".grid", function () {
-    //     let value = $(this).text()
-    //     alert("You click " + value)
-    // })
+window.addEventListener('load', function () {
 
     $("#clearButton").on("click", function () {
         $("#grids").html(" ")
     })
+
+    // preventing default  behaviours and stopping the events from bubbling up
+    ;
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false)
+    })
+
+    // adding highlight class on dragenter and dropover
+    ;
+    ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false)
+    })
+
+    // removing highlight class on dragenter and dropover
+    ;
+    ['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false)
+    })
+
 })
 
+// kicks off when user drops the file
+dropArea.addEventListener('drop', handleDrop, false)
+
 document.addEventListener('click', function (e) {
+    // let points = []
     if (e.target.classList.contains('grid')) {
         let btnId = e.target.getAttribute('id')
 
         let grid = document.getElementById(btnId)
         grid.style.transform = 'scale(0.85)'
         grid.style.background = '#01010c8b'
+
+        // points.push(e.target.x, e.target.y)
+        console.log(grid.offsetTop + ' ' + grid.offsetLeft)
     }
+    // console.log(points)
 })
 
 function generate() {
@@ -39,4 +62,43 @@ function generate() {
         }
     }
     $("#grids").html(content)
+}
+
+function preventDefaults(e) {
+    e.preventDefault()
+    e.stopPropagation()
+}
+
+
+function highlight(e) {
+    dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+    dropArea.classList.remove('highlight')
+}
+
+function handleDrop(e) {
+    let dt = e.dataTransfer
+    let files = dt.files
+
+    handleFiles(files)
+}
+
+function handleFiles(files) {
+    ([...files]).forEach(file => {
+        // console.log(file)
+        loadFile(file)
+    })
+}
+
+function loadFile(file) {
+    let reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = function () {
+        dropArea.style.backgroundImage = "url(" + reader.result + ")"
+        generate()
+        dropArea.style.height = 'auto'
+        dropArea.style.padding = '0'
+    }
 }
