c550bd8942c0abb5373899959de456281440d518
ZionLloyd05 <zionlloyd05@gmail.com>
2019-02-05 05:49:07 +0100

    clickables grid  added

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 window.addEventListener('load', function () {
     generate()
 
-    $("#grids").on("click", ".grid", function () {
-        let value = $(this).text()
-        alert("You click " + value)
-    })
+    // $("#grids").on("click", ".grid", function () {
+    //     let value = $(this).text()
+    //     alert("You click " + value)
+    // })
 
     $("#clearButton").on("click", function () {
         $("#grids").html(" ")
     })
 })
 
+document.addEventListener('click', function (e) {
+    if (e.target.classList.contains('grid')) {
+        let btnId = e.target.getAttribute('id')
+
+        let grid = document.getElementById(btnId)
+        grid.style.transform = 'scale(0.85)'
+        grid.style.background = '#01010c8b'
+    }
+})
+
 function generate() {
-    let value = 10
+    let value = 12
 
     let content = ""
     let num = 1
     for (let i = 1; i <= value; i++) {
         for (let j = 1; j <= 5; j++) {
             if (j === 1) {
-                content += "<div class='row'><div class='grid' id='grid" + num + "'>" + num + "</div>"
+                content += "<div class='row'><div class='grid' id='grid" + num + "'></div>"
             } else if (j === 5) {
-                content += "<div class='grid' id='grid" + num + "'>" + num + "</div></div>"
+                content += "<div class='grid' id='grid" + num + "'></div></div>"
             } else {
-                content += "<div class='grid' id='grid" + num + "'>" + num + "</div>"
+                content += "<div class='grid' id='grid" + num + "'></div>"
             }
             num++
         }
